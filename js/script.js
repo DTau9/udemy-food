07c25caca8f0ed30df6dc7e667109ea5c608a2c5
Dmitrij Tau <di.tau9@gmail.com>
2020-12-18 12:56:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,336 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	// =========================== Tabs =======================
+
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+	//прячем все вкладки
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+
+	//показываем вкладку
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	//делигируем события указателей вкладок
+	tabsParent.addEventListener('click', (e) => {
+		const target = e.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+	// ======================= Timer ============================
+
+	const deadLine = '2020-12-31';
+
+	function getTimeRemaining(endtime) {
+		//превращает строку во время(в мс) и возвращает разницу
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / (1000 * 60) % 60)),
+			seconds = Math.floor((t / 1000) % 60);
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	//поставить 0 перед числом
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		//запускаем, чтобы убрать мерцание при обновлении
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('.timer', deadLine);
+
+	// ============================ Modal ==================================
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal');
+
+
+	function openModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden'; //убираем прокрутку экрана
+		clearTimeout(modalTimerId);
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	}
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal || e.target.getAttribute('data-close') == '') {
+			closeModal();
+		}
+	});
+
+	//закрытие модального окна при принажатии Escape
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	const modalTimerId = setTimeout(openModal, 50000); //запуск окна по таймеру
+
+	//
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
+	//============================ Class ===========================
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUAH();
+		}
+
+		changeToUAH() {
+			this.price = this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement('div');
+
+			// проверяем наличие классов в ...classes
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	new MenuCard(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		9,
+		'.menu .container',
+		// сработает дефолтное значение
+	).render();
+
+	new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		14,
+		'.menu .container',
+		'menu__item'
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		21,
+		'.menu .container',
+		'menu__item'
+	).render();
+
+	//======================== Forms =================================
+
+	const forms = document.querySelectorAll('form');
+	// объект уведомлений о статусе отправки формы
+	const message = {
+		loading: 'img/form/spinner.svg',
+		success: 'Спасибо! скоро Мы с вами свяжемся',
+		failure: 'Что-то пошло не так'
+	};
+
+	// навешиваем функцию-обработчик на каждую форму
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	// функция отправки данных
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			// сформировать уведомление о статусе отправки формы
+			const statusMessage = document.createElement('img');
+			// показать при начале отправки
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`;
+			form.insertAdjacentElement('afterend', statusMessage);
+
+
+			const formData = new FormData(form);
+
+			// отправка через formData
+			/* // действия при загрузке запроса
+			fetch('server.php', {
+				method: 'POST',
+				// headers: { 'Content-type': 'application/json' },
+				body: formData
+			})
+				.then(data => data.text())
+				.then(data => {
+					console.log(data);
+					// показать при успешном запросе
+					showThanksModal(message.success);
+					statusMessage.remove();
+				})
+				.catch(() => showThanksModal(message.failure))
+				.finnaly(() => {
+					//очистить поле после ввода
+					form.reset();
+				}); */
+
+			// отправка через json
+			// перевод formData в json
+			const jsonObject = {};
+			formData.forEach(function (value, key) {
+				jsonObject[key] = value;
+			});
+
+			fetch('server.php', {
+				method: 'POST',
+				headers: { 'Content-type': 'application/json' },
+				body: JSON.stringify(jsonObject)
+			})
+				.then(data => data.text())
+				.then(data => {
+					console.log(data);
+					// показать при успешном запросе
+					showThanksModal(message.success);
+					statusMessage.remove();
+				})
+				.catch(() => showThanksModal(message.failure))
+				.finally(() => {
+					//очистить поле после ввода
+					form.reset();
+				});
+		});
+	}
+
+	function showThanksModal(message) {
+		const prevModalDiallog = document.querySelector('.modal__dialog');
+
+		prevModalDiallog.classList.add('hide');
+		openModal();
+
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('modal__dialog');
+		thanksModal.innerHTML = `
+			<div class="modal__content">
+				<div class="modal__close" data-close>×</div>
+				<div class="modal__title">${message}</div>
+			</div>
+		`;
+
+		document.querySelector('.modal').append(thanksModal);
+
+		// закрыть модальное окно после благодарности
+		setTimeout(() => {
+			thanksModal.remove();
+			prevModalDiallog.classList.add('show');
+			prevModalDiallog.classList.remove('hide');
+			closeModal();
+		}, 4000);
+	}
+
+	fetch('db.json')
+		.then((data) => data.json())
+		.then(res => console.log(res));//вернет объект
+
+});
---
+++ server.php
@@ -0,0 +1,5 @@
+<?php
+//позволяет работать с json
+$_POST = json_decode( file_get_contents("php://input"), true ); 
+
+echo var_dump($_POST);
